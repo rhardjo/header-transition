b045f4948689108241f8b09d9f85fe9f414a053d
Rano Hardjosemito <r.hardjo@live.nl>
2020-02-17 22:50:18 +0100

    First draft with parcel, react and tailwindcss

---
+++ postcss.config.js
@@ -0,0 +1,31 @@
+const tailwindcss = require('tailwindcss');
+
+const IS_DEV = process.env.NODE_ENV === 'development';
+
+const plugins = [tailwindcss];
+
+if (!IS_DEV) {
+  const purgecss = require('@fullhuman/postcss-purgecss');
+
+  class TailwindExtractor {
+    static extract(content) {
+      return content.match(/[A-z0-9-:\/]+/g) || [];
+    }
+  }
+
+  plugins.push(
+    purgecss({
+      content: ['src/*.html'],
+      extractors: [
+        {
+          extractor: TailwindExtractor,
+          extensions: ['html'],
+        },
+      ],
+    })
+  );
+}
+
+module.exports = {
+  plugins: plugins,
+};
---
+++ src/App.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const App = () => <div>Een opzetje voor de app</div>;
+
+export default App;
