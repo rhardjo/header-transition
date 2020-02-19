ee0c97e68f4e7f31710c7234d6752dd58a692d01
Rano Hardjosemito <r.hardjo@live.nl>
2020-02-19 21:21:03 +0100

    Use svg for menu icons

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,28 +1,37 @@
 import React from 'react';
 import cn from 'classnames';
 
+import CloseIcon from '../assets/svg/ico-close.svg';
+import MenuIcon from '../assets/svg/ico-menu.svg';
+
+import './Menu.pcss';
+
 const Menu = ({ items, isOpen, isButtonVisible, handleToggleMenu }) => {
-  const classname = cn('flex justify-end', {
+  const navClassname = cn('flex justify-end', {
     'opacity-100': isOpen,
     'opacity-0': !isOpen,
   });
-  const buttonClassname = cn('transition-all duration-150 h-12 bg-gray-800 rounded-sm text-white flex-none', {
+  const buttonClassname = cn('transition-all duration-150 h-12 rounded-sm text-white flex-none outline-none', {
     'w-0 opacity-0': !isButtonVisible,
-    'w-12 ml-10 opacity-100': isButtonVisible,
+    'w-12 ml-10 opacity-100 bg-gray-800': isButtonVisible,
   });
 
   return (
     <div className="inline-flex items-center">
-      <nav className={classname}>
+      <nav className={navClassname}>
         {isOpen &&
           items.map((item, key) => (
-            <div className="ml-16 text-lg font-bold tracking-wider text-white" key={key}>
+            <button className="ml-16 text-lg font-bold tracking-wider text-white" key={key}>
               {item}
-            </div>
+            </button>
           ))}
       </nav>
       <button className={buttonClassname} onClick={handleToggleMenu}>
-        {isOpen ? 'x' : '='}
+        {isOpen ? (
+          <CloseIcon className="Menu__icon Menu__icon--close" />
+        ) : (
+          <MenuIcon className="Menu__icon Menu__icon--burger" />
+        )}
       </button>
     </div>
   );
